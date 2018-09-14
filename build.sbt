67f32ea7493d7de1ad0330832442db44147708ed
Anton Rechenauer <antonrechenauer@gmail.com>
2018-09-14 12:23:00 +0200

    sql file added

--- build.sbt
+++ build.sbt
@@ -7,9 +7,9 @@ scalaVersion := "2.12.6"
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.5" % "test",
-  "com.typesafe" % "config" % "1.3.2",
+  "com.typesafe" % "config" % "1.3.3",
   "com.typesafe.slick" %% "slick" % "3.2.3",
-  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "org.slf4j" % "slf4j-nop" % "1.7.25",
   "com.typesafe.slick" %% "slick-hikaricp" % "3.2.3",
   "org.xerial" % "sqlite-jdbc" % "3.23.1"
 )
@@ -19,3 +19,9 @@ mainClass in (Compile, run) := Some("Starter")
 assemblyJarName in assembly := "ta2020.jar"
 
 mainClass in assembly := Some("Starter")
+
+scapegoatVersion in ThisBuild := "1.3.8"
+
+scalaBinaryVersion in ThisBuild := "2.12"
+
+wartremoverWarnings ++= Warts.all
--- src/main/scala/Starter.scala
+++ src/main/scala/Starter.scala
@@ -7,40 +7,35 @@ import scala.annotation.tailrec
 
 object Starter {
   def main(args: Array[String]): Unit = {
-    val fs = getListOfAllowedFiles(Configuration.directory,allowed).map(_.getAbsolutePath)
-    fs.foreach{ f=>
-      val len = f.length
-      val lenstr = f"$len%4s"
-      println(s"$lenstr;$f")
+
+    if (args.contains("help")) println("Scan files relevat for TA2020 MUO")
+
+    val fs = getListOfAllowedFiles(Configuration.directory, allowed).map(_.getAbsolutePath)
+    fs.foreach { f =>
+      val len = f.length.toString
+      val lenstr: String = s"$len%4s"
+      println(lenstr)
     }
     //ta2020.TADatabase.create
 
   }
 
-  def allowed(f:File): Boolean = {
-    if (null==f) false else {
-      val fname = f.getName
-      List(".xls", ".xlsx", ".pdf").exists(fname.endsWith)
-    }
+  def allowed(file: File): Boolean = Option(file) match {
+    case None => false
+    case Some(f) => f.isFile && List(".xls", ".xlsx", ".pdf").exists(f.getName.endsWith)
   }
 
   def getListOfAllowedFiles(dir: List[String], pred: File => Boolean): List[File] = {
 
     @tailrec def scanDirs(dirs: List[File], files: List[File]): List[File] = dirs match {
       case Nil => files
-      case head :: rest =>
-        val liste = head.listFiles
-        val newfiles: List[File] = {
-          if(null==liste) Nil else head.listFiles.filter(f => pred(f)).toList
-        }
-        val newdirs: List[File] = {
-          if(null==liste) Nil else head.listFiles.filter(_.isDirectory).toList
-        }
-        scanDirs(newdirs ::: rest, newfiles ::: files)
+      case head :: rest => Option(head.listFiles) match {
+        case None => scanDirs(rest, files)
+        case Some(l) => scanDirs(l.filter(_.isDirectory).toList ::: rest, l.filter(f => pred(f)).toList ::: files)
+      }
     }
-
     val df = dir.map(new File(_)).filter(_.isDirectory)
-   scanDirs(df, Nil)
+    scanDirs(df, Nil)
 
   }
 
